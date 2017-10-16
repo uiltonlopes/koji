a8784cf9008129369823704a600bada734e4267a
uiltonlopes <uilton@sweettecnologia.com>
2017-10-16 16:25:57 -0300

    Subindo projeto para git

---
+++ js/init.js
@@ -0,0 +1,14 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.slider').slider({height: 550});
+    $('select').material_select();
+
+
+
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
